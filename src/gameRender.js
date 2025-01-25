bb852fe99b85d69b162db23a2d5e1887baade171
Sergio Galdos <galdos.sergio@outlook.com>
2025-01-25 03:20:55 +0100

    [dev][update] v.1 finish

--- src/gameRender.js
+++ src/gameRender.js
@@ -139,10 +139,10 @@ function drawSprites() {
     renderSprite(sprite)
 
     // TEST: dibuja el hitbox
-    drawHitBox(sprite)
+    //drawHitBox(sprite)
 
     // TEST: dibuja el hitbox del hacha
-    drawStrikeBox(sprite)
+    //drawStrikeBox(sprite)
   }
 }
 
